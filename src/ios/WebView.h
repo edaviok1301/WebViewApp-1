f3901f99e0f04a4b9dbd8c2f07c54e703c2b5a76
Gerson Peralta <gerson.jp.lass@gmail.com>
2020-07-20 12:08:27 -0500

    First commit

---
+++ src/ios/WebView.h
@@ -0,0 +1,11 @@
+
+#import <UIKit/UIKit.h>
+#import <Cordova/CDV.h>
+
+@interface WebView : CDVPlugin {
+  // Member variables go here.
+}
+
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
